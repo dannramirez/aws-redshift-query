5cc7e1f9b105f2534c69792c7e288ae062237acb
Dann Ramírez <68350246+dannramirez@users.noreply.github.com>
2022-12-11 11:36:28 -0600

    add: Get schema permissions too

--- get-permissions.sql
+++ get-permissions.sql
@@ -10,7 +10,7 @@ SELECT pg_get_userbyid(b.relowner)::text AS objowner,
 		(SELECT oid,generate_series(1,array_upper(relacl,1))  AS n FROM pg_catalog.pg_class) NS
 		INNER JOIN pg_catalog.pg_class B ON b.oid = ns.oid AND  NS.n <= array_upper(b.relacl,1)
 		INNER JOIN pg_catalog.pg_namespace c on b.relnamespace = c.oid
-		where relkind in ('r','v')
+		where relkind in ('r','v') and schemaname != 'pg_catalog' and schemaname != 'information_schema' and objowner!='rdsdb'
 UNION ALL
 ---- table and view column privileges
 SELECT pg_get_userbyid(c.relowner)::text AS objowner, 
@@ -25,7 +25,7 @@ SELECT pg_get_userbyid(c.relowner)::text AS objowner,
 		INNER JOIN pg_catalog.pg_attribute_info B ON b.attrelid = ns.attrelid AND  NS.n <= array_upper(b.attacl,1)
 		INNER JOIN pg_catalog.pg_class c on b.attrelid = c.oid
 		INNER JOIN pg_catalog.pg_namespace d on c.relnamespace = d.oid
-		where relkind in ('r','v')
+		where relkind in ('r','v') and schemaname != 'pg_catalog' and schemaname != 'information_schema' and objowner!='rdsdb'
 UNION ALL
 SELECT pg_get_userbyid(b.nspowner)::text AS objowner,
 		null::text AS schemaname,
@@ -39,5 +39,4 @@ SELECT pg_get_userbyid(b.nspowner)::text AS objowner,
 		INNER JOIN pg_catalog.pg_namespace B ON b.oid = ns.oid AND NS.n <= array_upper(b.nspacl,1)
 )
 
-SELECT  objowner,schemaname,objname,objtype,aclstring FROM objprivs
-WHERE schemaname != 'pg_catalog' and schemaname != 'information_schema' order by schemaname,objname,objtype asc;
+SELECT * FROM objprivs where objowner!='rdsdb' order by schemaname,objname,objtype asc;
